24095da37e385133db76cfaf8f423534584aba36
ellis <elrestea@gmail.com>
2025-11-06 17:08:35 +0000

    finished num 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+   
+    while (num1 !== num2){
+        num1 += 1
+    }
+    
+    return num1 + num2 + 1;
+    
 };
 
 // Do not edit below this line
