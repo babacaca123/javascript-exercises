7f727c41765d222acb40cdc7822a4b08144a3977
ellis <elrestea@gmail.com>
2025-11-06 16:32:24 +0000

    did the exercise cause im chill like that

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+   
+    rString = string.split("")
 
+    rString.reverse();
+
+    reversed = rString.join("");    
+
+    return reversed;
 };
 
 // Do not edit below this line
