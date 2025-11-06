9b3c5358958b6ecc300cb7bd6a62a67f5210d342
ellis <elrestea@gmail.com>
2025-11-06 16:56:41 +0000

    did the exercise that is number 8, Im smart :D

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, r) {
+
+
+ for(let i = 0; i < array.length; i++) {
+    if (array[i] === r) { 
+        array.splice(i, 1);
+        i--;
+    }
+    
+}   
+return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
