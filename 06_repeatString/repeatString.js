82789e4a6d75606e08c6e7b96256ce2418b6fc81
ellis <elrestea@gmail.com>
2025-11-06 16:16:44 +0000

    did the exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    let rString = "";
 
+    while (num > 0){
+        rString += string;
+        num--;
+    }
+    return rString;
 };
 
 // Do not edit below this line
