72b3f9504f34df4f5552fe89db518bb83f1f2ab3
ellis <elrestea@gmail.com>
2026-02-02 19:29:12 +0000

    do fibonacci :D this is good!

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
 
+
+    let fibo = [0, 1]
+    x = 0;
+    for(let i = 1; i < a ; i++){
+        x = fibo[i] + fibo[i -1]
+        fibo.push(x);
+    }
+    return  a < 0 ? "OOPS" : fibo[a];
 };
 
 // Do not edit below this line
