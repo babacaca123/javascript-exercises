3472fb65c8eff2905cd8a6c2ad31405091b65fcd
ellis <elrestea@gmail.com>
2026-02-02 15:32:40 +0000

    passed :D

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let clean = str.toLowerCase().replace(/[^a-z0-9]/g, '')
+    let reverse = ''
+    
 
+    for(let i = clean.length - 1; i >= 0; i--){
+        reverse += clean[i];
+        
+    }
+    return clean === reverse;
 };
 
 // Do not edit below this line
