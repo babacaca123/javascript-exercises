4f3d1cee485219018490cda1fc1ba9f9ee722335
ellis <elrestea@gmail.com>
2026-02-03 00:02:46 +0000

    finsihed java exercises :O i did it :D

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    const oldest = people.reduce((oldestSoFar, currentPerson) =>{
+
+        let oldestAge;
+        if(!oldestSoFar.yearOfDeath){
+            oldestAge = new Date().getFullYear() - oldestSoFar.yearOfBirth;
+        }
+        else{
+            oldestAge = oldestSoFar.yearOfDeath - oldestSoFar.yearOfBirth;
+        }
+
+
+        let currentAge;
+        if(!currentPerson.yearOfDeath){
+            currentAge = new Date().getFullYear() - currentPerson.yearOfBirth;
+        }
+        else{
+            currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        }
+
+
+        if(currentAge > oldestAge){
+            return currentPerson;
+
+        } 
+        else{
+            return oldestSoFar;
+        }
+    })
+    return oldest;
 };
 
 // Do not edit below this line
